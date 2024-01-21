bc3f967539a1460f8f7b7177b84f2209da8d8062
Dmitrii Doronin <doronin_dima@mail.ru>
2024-01-21 16:55:58 +0300

    add ts

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from 'express'
-import { averageOfLevels } from './Tree/BFS/AverageBinaryTree.js'
 
-const app = express()
-const port = 3000
-
+const app = express();
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is working on port ${port}`)
 })
