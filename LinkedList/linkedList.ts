c95d5fed1d30ad56e1873b6ac9b0b6449fc7fca2
Dmitrii Doronin <doronin_dima@mail.ru>
2024-01-30 19:17:52 +0300

    linked list

--- LinkedList/linkedList.ts
+++ LinkedList/linkedList.ts
@@ -66,6 +66,7 @@ export class LinkedList {
     return currentHead;
   }
   unshift(val: unknown) {
+
     let newNode = new Node(val);
 
     if (!this.head) {
@@ -76,6 +77,62 @@ export class LinkedList {
       this.head = newNode;
     }
     this.length++;
+    return this;
+  }
+  get(index: number) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+
+    while (counter !== index) {
+      current = current?.next;
+      counter++;
+    }
+    return current;
+  }
+  set(index: number, val: unknown) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index: number, val: unknown) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return this;
+  }
+  remove(index: number) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
     return this;
   }
 }
