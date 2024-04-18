28de254adb38736bd559078a8f9e3bc52650ff99
DoroninDmitrii <doronin_dima@mail.ru>
2024-04-18 19:34:45 +0300

    56mergeIntervals

--- Intervals/56metgeIntervals.js
+++ Intervals/56metgeIntervals.js
@@ -1,22 +1,27 @@
-const intervals = [[1,3],[2,6],[8,10],[15,18]];
+const intervals = [[1, 3], [2, 6], [8, 10], [15, 18]];
 
 
 const mergeIntervals = (intervals) => {
-  
-  let result = [intervals[0]];
-  let current = result[0];
-  
+
+  if (intervals.length === 0) {
+    return [];
+  }
+
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const mergedIntervals = [intervals[0]];
+
   for (let i = 1; i < intervals.length; i++) {
-    const next = intervals[i];
-    
-    if (current[1] > next[0]) {
-      current[1] = Math.max(current[1], next[1]);
+    const currentInterval = intervals[i];
+    const lastMergedInterval = mergedIntervals[mergedIntervals.length - 1];
+
+    if (currentInterval[0] > lastMergedInterval[1]) {
+      mergedIntervals.push(currentInterval);
     } else {
-      current = next;
-      result.push(current);
+      lastMergedInterval[1] = Math.max(lastMergedInterval[1], currentInterval[1]);
     }
   }
-  return result;
+  return mergedIntervals;
 }
 
 console.log(mergeIntervals(intervals))
