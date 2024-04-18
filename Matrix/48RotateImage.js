a2e1877eaf8794935713d9b8da829e522058114e
DoroninDmitrii <doronin_dima@mail.ru>
2024-04-18 19:10:09 +0300

    48rotateimage

---
+++ Matrix/48RotateImage.js
@@ -0,0 +1,17 @@
+const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+const rotate = function (matrix) {
+  matrix.reverse();
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < row; col++) {
+      const temp = matrix[row][col];
+      matrix[row][col] = matrix[col][row];
+      matrix[col][row] = temp;
+    }
+  }
+  return matrix;
+};
+
+console.log(rotate(matrix));
