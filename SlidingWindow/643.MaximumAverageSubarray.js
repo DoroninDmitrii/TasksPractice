53834f4ab6bcc0a2ac63b8211e33c269837d75da
DoroninDmitrii <doronin_dima@mail.ru>
2024-05-12 21:59:29 +0300

    643 sliding wind

---
+++ SlidingWindow/643.MaximumAverageSubarray.js
@@ -0,0 +1,22 @@
+const nums = [1,12,-5,-6,50,3,5,7];
+const k = 4;
+// Output: 12.75000
+
+const findMaxAverage = function(nums, k) {
+    let sum = 0;
+
+    for (let i = 0; i < k; i++) {
+      sum += nums[i];
+    }
+
+    let result = sum;
+
+    for (let i = k; i < nums.length; i++) {
+      sum += nums[i] - nums[i - k];
+      result = Math.max(result, sum);
+    }
+
+    return result / k;
+};
+
+console.log(findMaxAverage(nums, k));
