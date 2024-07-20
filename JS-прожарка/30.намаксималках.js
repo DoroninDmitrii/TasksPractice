3f72f5f56dde1952c4cbe1dce4ca41cd0fe2d68e
DoroninDmitrii <doronin_dima@mail.ru>
2024-07-20 22:55:00 +0300

    30 proz

--- JS-прожарка/30.намаксималках.js
+++ JS-прожарка/30.намаксималках.js
@@ -1,10 +1,18 @@
 const getLocalMax = (arr, localeSize) => {
 
+  let result = [];
+
+  for (let i = 0; i <= arr.length - localeSize; i++) {
+    let max = 0;
+    for (let k = 0; k < localeSize; k++) {
+      if (arr[i + k] > max) {
+        max = arr[i + k];
+      }
+    }
+    result.push(max);
+  }
+  return result;
 };
-
-
-
-
 
 console.log(getLocalMax([1, 3, -1, 5, 7], 3));
 // [3, 5, 7]
