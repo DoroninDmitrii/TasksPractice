02a85fa74b2c0c06787bf2d2c1205bbd7624d8c0
DoroninDmitrii <doronin_dima@mail.ru>
2024-07-23 21:58:21 +0300

    34 event emit

--- JS-прожарка/34.EventEmitter.js
+++ JS-прожарка/34.EventEmitter.js
@@ -10,11 +10,35 @@
 // }
 
 class EventEmitter {
-  // реализуй класс EventEmitter
+  constructor() {
+    this.events = {};
+  }
+
+  subscribe(typeEvent, callback) {
+    if (!this.events[typeEvent]) {
+      this.events[typeEvent] = [];
+    }
+    this.events[typeEvent].push(callback);
+  }
+
+  unsubscribe(typeEvent, callback) {
+    if (!this.events[typeEvent]) return false;
+    
+    const index = this.events[typeEvent].indexOf(callback);
+    if (index > -1) {
+      this.events[typeEvent].splice(index, 1);
+      return true;
+    }
+    return false;
+  }
+
+  emit(typeEvent, data) {
+    if (!this.events[typeEvent]) return false;
+
+    this.events[typeEvent].forEach((callback) => callback(data));
+    return true;
+  }
 }
-
-
-
 
 
 const eventEmitter = new EventEmitter();
@@ -23,16 +47,19 @@ const handleClick = (payload) => {
   console.log({ event: 'click', payload, fnName: handleClick.name });
 }
 
-eventEmitter.subscribe('click', handleClick);
-eventEmitter.subscribe('click', (payload) => console.log({ event: 'click', payload, fnName: 'anonymus' }));
-eventEmitter.emit('click', 'click-payload');
-/* 
+console.log(eventEmitter.subscribe('click', handleClick));
+// console.log(eventEmitter.subscribe('click2', handleClick));
+
+// eventEmitter.subscribe('click', (payload) => console.log({ event: 'click', payload, fnName: 'anonymus' }));
+// eventEmitter.emit('click', 'click-payload');
+/*
   { event: 'click', payload: 'click-payload', fnName: 'handleClick' }
   { event: 'click', payload: 'click-payload', fnName: 'anonymus' }
 */
 
-eventEmitter.unsubscribe('click', handleClick);
-eventEmitter.emit('click', 'click-payload-2');
+console.log(eventEmitter.unsubscribe('click', handleClick));
+
+console.log(eventEmitter.emit('click', 'click-payload-2'));
 /* 
   { event: 'click', payload: 'click-payload', fnName: 'anonymus' }
 */
--- JS-прожарка/35.ПлюсуемвФункции.js
+++ JS-прожарка/35.ПлюсуемвФункции.js
@@ -3,21 +3,22 @@
 // 9
 
 function sum(value) {
-  let total = value;
+  let total = value ?? 0;
 
   function innerSum(args) {
-    if (args === undefined) {
-      return total;
-    } else {
-      total += args;
+      total += args ?? 0;
 
       return innerSum;
     }
+
+  innerSum.toString = function () {
+    return total;
   }
+
   return innerSum;
 };
 
 
-console.log(sum(5)(-1)(5)()); // 9
-// sum(2)(1)()()()()(10)()(-100); --> -87
+console.log(Number(sum(5)(-1)(5)())); // 9
+console.log(Number(sum(2)(1)()()()()(10)()(-100)())); // -87
 // sum(); --> 0
