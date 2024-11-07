71db4e5d45baf080d4f3f88fe79c4b6572e34716
DoroninDmitrii <doronin_dima@mail.ru>
2024-11-07 21:47:14 +0300

    check commit

---
+++ JS-прожарка/108.сжатие.js
@@ -0,0 +1,21 @@
+const compress = (nums) => {
+  const sortNums = nums.sort((a, b) => a - b);
+  
+  let pointer = 0;
+  const result = [];
+  
+  for (let i = 0; i < sortNums.length; i++) {
+    if (nums[i + 1] - nums[i] !== 1) {
+      if (nums[pointer] === nums[i]) {
+        result.push(String(nums[i]));
+      } else {
+        result.push(`${nums[pointer]}-${nums[i]}`);
+      }
+      pointer = i + 1;
+    }
+  }
+  return result;
+}
+
+
+console.log(compress([3, 2, 1, 5, 6, -1, 10])); // '-1,1-3,5-6,10' 
