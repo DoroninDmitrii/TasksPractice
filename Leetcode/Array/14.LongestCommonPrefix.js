5e1e438e39a81d2bfd5ae8fda681dd0328b52ec0
doronin <doronin@tochka.com>
2026-07-05 21:24:43 +0300

    14 and 15

--- Leetcode/Array/14.LongestCommonPrefix.js
+++ Leetcode/Array/14.LongestCommonPrefix.js
@@ -1,4 +1,4 @@
-const strs = ["bat","bag","bank","band"]
+// const strs = ["bat","bag","bank","band"]
 
 // Output: "ba"
 // Example 2:
@@ -7,16 +7,35 @@ const strs = ["bat","bag","bank","band"]
 
 // Output: "da"
 
-const longestCommonPrefix = (strs) => {
-        for (let i = 0; i < strs[0].length; i++) {
-            for (let s of strs) {
-                if (i === s.length || s[i] !== strs[0][i]) {
-                    return s.slice(0, i).length;
-                }
-            }
-        }
-    return strs[0];
-}
+// const longestCommonPrefix = (strs) => {
+//         for (let i = 0; i < strs[0].length; i++) {
+//             for (let s of strs) {
+//                 if (i === s.length || s[i] !== strs[0][i]) {
+//                     return s.slice(0, i).length;
+//                 }
+//             }
+//         }
+//     return strs[0];
+// }
 
+
+// console.log(longestCommonPrefix(strs));
+
+const strs = ["flower", "flow", "flight"];
+
+function longestCommonPrefix(strs) {
+  if (!strs.length) return "";
+
+  for (let i = 0; i < strs[0].length; i++) {
+    const current = strs[0][i];
+
+    for (let k = 1; k < strs.length; k++) {
+      if (strs[k][i] !== current) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+  return strs[0];
+}
 
 console.log(longestCommonPrefix(strs));
--- Leetcode/TwoPointer/15.3sum.js
+++ Leetcode/TwoPointer/15.3sum.js
@@ -1,53 +1,90 @@
+// const nums = [-1, 0, 1, 2, -1, -4];
+
+// const threeSum = (nums, target = 0) => {
+
+//   const result = [];
+
+//   if (nums.length < 3) {
+//     return result;
+//   }
+
+//   nums.sort((a, b) => a - b);
+
+//   for (let left = 0; left < nums.length - 2; left++) {
+
+//     if (nums[left] > target) break;
+
+//     if (left > 0 && nums[left] === nums[left - 1]) {
+//       continue;
+//     }
+
+//     let middle = left + 1;
+//     let right = nums.length - 1;
+
+//     while (middle < right) {
+//       const sum = nums[left] + nums[middle] + nums[right];
+
+//       if (target === sum) {
+//         result.push([nums[left], nums[middle], nums[right]]);
+
+//         while(nums[middle] === nums[middle + 1]) middle++;
+//         while(nums[right] === nums[right + 1]) right++;
+
+//         middle++;
+//         right--;
+//         continue;
+//       }
+
+//       if (sum < target) {
+//         middle++;
+//         continue;
+//       }
+
+//       if (sum > target) {
+//         right--;
+//         continue;
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(threeSum(nums));
+
+
 const nums = [-1, 0, 1, 2, -1, -4];
 
-const threeSum = (nums, target = 0) => {
-
-  const result = [];
-
-  if (nums.length < 3) {
-    return result;
-  }
+const threeSum = (nums) => {
+  const res = [];
 
   nums.sort((a, b) => a - b);
 
-  for (let left = 0; left < nums.length - 2; left++) {
-
-    if (nums[left] > target) break;
-
-    if (left > 0 && nums[left] === nums[left - 1]) {
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
       continue;
     }
 
-    let middle = left + 1;
-    let right = nums.length - 1;
+    let left = i + 1;
+    let rigth = nums.length - 1;
 
-    while (middle < right) {
-      const sum = nums[left] + nums[middle] + nums[right];
+    while (left < rigth) {
+      let total = nums[i] + nums[left] + nums[rigth];
 
-      if (target === sum) {
-        result.push([nums[left], nums[middle], nums[right]]);
+      if (total > 0) {
+        rigth--;
+      } else if (total < 0) {
+        left++;
+      } else {
+        res.push([nums[i], nums[left], nums[rigth]]);
+        left++;
 
-        while(nums[middle] === nums[middle + 1]) middle++;
-        while(nums[right] === nums[right + 1]) right++;
-
-        middle++;
-        right--;
-        continue;
-      }
-
-      if (sum < target) {
-        middle++;
-        continue;
-      }
-
-      if (sum > target) {
-        right--;
-        continue;
+        while (nums[left] === nums[left - 1] && left < rigth) {
+          left++;
+        } 
       }
     }
   }
-  return result;
+  return res;
 }
 
 console.log(threeSum(nums));
-
